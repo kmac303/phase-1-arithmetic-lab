d5a140e3147ef95cceff0317f2422f15242bd0d9
Kevin McIntosh <kmac303@gmail.com>
2022-06-12 10:54:45 -0600

    commit tree e3fd685be83728dec538b3df0227fbd668ac904f

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+// function multiply(num1, num2) {
+//     return num1 * num2;
+// }
 
-// Write your code here
+// multiply(31, 2);
+
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random()) + 1;
+
+let num3 = 21
+let num4 = 17
+let mod = num3 % num4;
+
+let max = Math.max(18, 19, 20);
